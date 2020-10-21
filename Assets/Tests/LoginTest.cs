f6c5ae690d0eb4333ad54643964654e9529b2c78
logansisel <56094407+logansisel@users.noreply.github.com>
2020-10-21 18:36:20 -0500

    added test scripts

---
+++ Assets/Tests/LoginTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LoginTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void LoginTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            // Ex: Assert.AreEqual("hello", "hello");
+
+            // NOTE: Starts with known user in database,
+            // fails if user doesn't already exsist
+
+
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator LoginTestWithEnumeratorPasses()
+        {
+            // NOT USED FOR THIS TEST
+
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Tests/RegisterTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class RegisterTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void RegisterTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            // Ex: Assert.AreEqual("hello", "hello");
+
+            // TODO: Call registration method
+            
+
+            // TODO: Get info from database
+            // query database
+
+
+            // TODO: Check that registration info matches database entry
+            // check information exsists
+            // check information matches
+
+
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator RegisterTestWithEnumeratorPasses()
+        {
+            // NOT USED FOR THIS TEST
+
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
