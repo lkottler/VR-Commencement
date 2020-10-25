776fa9e3649979af07e5320e33a0c55b7651a1ef
zding52 <43459247+zding52@users.noreply.github.com>
2020-10-24 23:08:56 -0500

    CharacterMovement
    Added Script CharacterMovement.cs to move character by WASD

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+   
+	[Header("Settings")]
+	public float speed = 10f;
+	public CharacterController controller;
+	
+
+	void Awake()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 forward = transform.forward * v * speed * Time.deltaTime;
+		Vector3 right = transform.right * h * speed * Time.deltaTime;
+
+		controller.Move(forward + right);
+	}
+}
