f582c8d8b0f62188a36906c5d42c015f0307cdb9
zding52 <43459247+zding52@users.noreply.github.com>
2020-10-27 20:57:38 -0500

    Added Photon (first connection) & Decoration
    Photon is installed and test connected. No farther connect setting yet. Added images as decoration. Can't find good enough 3d model online.

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    private void Start() 
+    {
+        print("Connecting to server.");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+        //PhotonNetwork.connectToBestCloudServer();
+    }
+    
+    public override void OnConnectedToMaster() 
+    {
+        print("Connected to server.");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) 
+    {
+        print("Disconnected from server for reason" + cause.ToString());
+    }
+
+ 
+}
+
