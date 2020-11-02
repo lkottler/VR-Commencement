92546a7b7e364b0ecb2d8a923da246780c81f3ad
Logan Kottler <lkottler@wisc.edu>
2020-11-02 17:55:16 -0600

    gitignore wont ignore these

---
+++ Assets/Tests/HomePage.cs
@@ -0,0 +1,104 @@
+﻿//using Photon.Pun.Demo.Cockpit;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class HomePage : MonoBehaviour
+{
+    public InputField regUser, regPW, regConf, logUser, logPW;
+    public GameObject MenuCanvas;
+
+    public GameObject player;
+    public Camera loginCam;
+
+    public Button loginBtn;
+    public Button regBtn;
+    public Button guestBtn;
+
+    public bool loginSuccess;
+    public bool regSuccess;
+
+    public bool CallRegister()
+    {
+        regSuccess = false;
+        StartCoroutine(Register());
+        return regSuccess;
+    }
+
+    public void SpawnPlayer()
+    {
+        GameObject generatedPlayer = Instantiate(player, new Vector3(-149.5f, 126.215f, 262.58f), Quaternion.identity);
+        loginCam.enabled = false;
+        //generatedPlayer.transform.localScale = new Vector3(3, 3, 3);
+        MenuCanvas.SetActive(false);
+
+    }
+    public void CallGuest()
+    {
+        SpawnPlayer();
+    }
+
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", regUser.text);
+        form.AddField("password", regPW.text);
+        string url = "http://pages.cs.wisc.edu/~lkottler/commencement/register"; //TODO
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, form))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.downloadHandler.text[0] == '0')
+            {
+                Debug.Log("Registration worked.");
+                SpawnPlayer();
+                regSuccess = true;
+            }
+            else
+            {
+                Debug.Log("Registration failed. Error #" + webRequest.downloadHandler.text);
+            }
+        }
+
+    }
+
+    public bool CallLogin()
+    {
+        loginSuccess = false;
+        StartCoroutine(Login());
+        return loginSuccess;
+    }
+
+    IEnumerator Login()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", logUser.text);
+        form.AddField("password", logPW.text);
+        string url = "http://pages.cs.wisc.edu/~lkottler/commencement/login"; //TODO
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(url, form))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.downloadHandler.text[0] == '0')
+            {
+                Debug.Log("Login worked.");
+                SpawnPlayer();
+                loginSuccess = true;
+            }
+            else
+            {
+                Debug.Log("Login failed. Error #" + webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    public void VerifyInputs()
+    {
+        regBtn.interactable = (regPW.text != regConf.text) ? false : regUser.text.Length > 4 && regPW.text.Length > 4;
+        loginBtn.interactable = logUser.text.Length > 4 && logPW.text.Length > 4;
+    }
+
+}
