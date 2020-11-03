5cfa8824ce3dae86f23f0cfd0566d3efa9b5b4e3
Logan Kottler <lkottler@wisc.edu>
2020-11-02 19:57:01 -0600

    Implemented Photon connection with Player prefab

---
+++ Assets/Scripts/Networking/QuickSpawnRoomController.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class QuickSpawnRoomController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex; // Build index of multiplayer scene
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room");
+        StartGame();
+    }
+    
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+
+}
