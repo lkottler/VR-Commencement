d1b4cf8223fb8c529772246f0c565ba9bd28b3ad
zding52 <43459247+zding52@users.noreply.github.com>
2020-11-12 01:46:49 -0600

    Added Dialogue function in a testing scene Chancellor_test

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue() 
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        //FindObjectOfType<DiallogueManager>().StartDialogue(dialogue);
+    }
+}
+
+
---
+++ Assets/Scripts/Dialogue/Instruction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instruction : MonoBehaviour
+{
+    public KeyCode _key;
+    public Button _button;
+    //public DialogueTrigger dialogueTrigger;
+    void Update()
+    {
+        if (Input.GetKeyDown(_key))
+        {
+            _button.onClick.Invoke();
+            Debug.Log("key pressed.");
+
+        }
+
+    }
+}
+
+
