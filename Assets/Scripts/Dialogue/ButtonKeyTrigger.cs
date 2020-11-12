60ed6ca07188926f133de07311ae5378e918083f
zding52 <43459247+zding52@users.noreply.github.com>
2020-11-12 02:05:25 -0600

    Dialogue function
    Dialogue works, I added an instruction, triggered by pressing tab, continue with enter, and short animation to display and hide the dialogue box https://www.youtube.com/watch?v=_nRzoTzeyxU

---
+++ Assets/Scripts/Dialogue/ButtonKeyTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonKeyTrigger : MonoBehaviour
+{
+
+    public KeyCode _key;
+    public  Button _button;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_key)) 
+        {
+            _button.onClick.Invoke();
+            Debug.Log("key pressed.");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Dialogue/Chancellor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Allows the player to control a character from the Synty Studios "Simple People" asset pack.
+/// </summary>
+public class ChancellorController : MonoBehaviour
+{
+    // Member variables.
+    private Animator animator;
+    private bool isMoving = false;
+
+    /// <summary>
+    /// Performs initialisation of this component.
+    /// </summary>
+    public void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+        if (this.animator != null)
+        {
+            // Set the character animation to idle and enable non-static animations.
+            this.animator.SetFloat("Speed_f", 0.0f);
+            //this.animator.SetBool("Static_b", false);
+        }
+    }
+}
