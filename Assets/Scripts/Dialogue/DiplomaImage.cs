14fc33d82d98ac031357b85fbed95e687c6b9d2e
zding52 <43459247+zding52@users.noreply.github.com>
2020-11-13 20:22:03 -0600

    Chancellor handling diploma and talk

---
+++ Assets/Scripts/Dialogue/DiplomaImage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiplomaImage : MonoBehaviour
+{
+    public Image image;
+    public Text userName;
+    public Text userMajor;
+    public Text instruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.enabled = false;
+        userName.enabled = false;
+        userMajor.enabled = false;
+        instruction.enabled = false;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+      
+            Debug.Log("key G pressed.");
+            image.enabled = !image.enabled;
+            userName.enabled = image.enabled;
+            userMajor.enabled = image.enabled;
+            instruction.enabled = image.enabled;
+        }
+    }
+}
