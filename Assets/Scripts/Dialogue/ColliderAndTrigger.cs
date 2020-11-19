268fa5ca6660774d90f24d716c4520da5a87e9ef
zding52 <43459247+zding52@users.noreply.github.com>
2020-11-19 13:01:10 -0600

    Fixed diploma bug
    rn Guest can't claim diploma, if you login, you could

--- Assets/Scripts/Dialogue/ColliderAndTrigger.cs
+++ Assets/Scripts/Dialogue/ColliderAndTrigger.cs
@@ -7,16 +7,47 @@ public class ColliderAndTrigger : MonoBehaviour
 {
     public DialogueTrigger dialogueTrigger;
     public Text DiplomaInstruction;
+
     void Start() 
     {
         DiplomaInstruction.enabled = false;
     }
     private void OnTriggerEnter(Collider other) 
     {
-        Debug.Log(other.name);
+        //Collider gameobject PLAYER(CLONE)
+        Debug.Log(other.name + ", " + UserStats.getUsername());
+        GameObject otherObject = other.gameObject;
+
+        //what it suppose to be
+        Debug.Log("userName: " + UserStats.getUsername());
+
+
+        //Transform CollideName = otherObject.transform.Find("Username");//Get the UserName Transform
+        //TODO
+        Text otherNameObject = otherObject.GetComponent<Text>();
+        Debug.Log("NameObject: " + otherNameObject);
+
+
+        string childerenname = otherObject.GetComponentInChildren<TextMesh>().text;
+        Debug.Log("childeren: " + childerenname);
+       
+
+
+        //string NameText = otherObject.GetComponent<UnityEngine.UI.Text>().text;
+        //Debug.Log("NameText: " + NameText);
+        //Debug.Log("CollideName: " + CollideName);
+        //Debug.Log("Text: " + CollideName.toString);
+        //Debug.Log("TextMesh: " + CollideName.textMesh);
+
+        // Text oldText = GameObject.Find("Username").GetComponent<Text>();
+        //Debug.Log("Text:" + oldText);
+        //Debug.Log("Text:" + oldText.text);
+
+
+        if (childerenname == UserStats.getUsername().ToString()) 
+        {
         dialogueTrigger.TriggerDialogue();
         DiplomaInstruction.enabled = true;
-
-      
+        }      
     }
 }
