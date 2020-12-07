6a102b88a41b65c15b33118247a0cacda2d22f90
Logan Kottler <lkottler@wisc.edu>
2020-12-07 05:08:19 -0600

    Added soccer
    ball + rigidbody + physics, also goals

---
+++ Assets/ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public GameObject stadium;
+    private float kickForce = 20f;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Physics.IgnoreCollision(stadium.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name.Contains("Clone"))
+        {
+            Vector3 direction = (other.transform.position - transform.position).normalized;
+            rb.AddForce(-direction * kickForce, ForceMode.Impulse);
+        }
+    }
+}
